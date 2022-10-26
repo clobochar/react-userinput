094eb2644cd2b78d55472966d0deaa4f80946ac7
Laura Clobochar Pereira <clo.bochar_@hotmail.com>
2022-10-26 11:41:07 +0100

    Add handleChange

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState("")
+  const [id, setId] = useState("")
+
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => res.json()).then((res) => setUser(res))
+  }, [id])
+
+  function handleChange(e) {
+    const input = e.target.value;
+    if(Number(input) || input === "" ) {
+      setId(input)
+  }
+    };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input placeholder='Insert a number ID' onChange={handleChange} value={id}/>
+       <div>
+        <h1>{user.name}</h1>
+        <p>{user.username}</p>
+        <p>{user.phone}</p>
+       </div>
     </div>
   )
 }
 
-export default App
+export default App;
